5f8c4df7aa85d0faab4f0678cb53ef63a8d9c030
Davlatbek Kobiljonov Sherzodovich <117518580+Davlatbek0710@users.noreply.github.com>
2024-04-20 14:28:53 +0500

    Automated Amazon Product Price Tracker.py

--- Automated Amazon Product Price Tracker.py
+++ Automated Amazon Product Price Tracker.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import lxml
 from smtplib import SMTP
 
-
+YOUR_EMAIL = YOUR EMAIL
+PASSWORD = YOUR PASSWORD
 header = {
     "User-Agent":"Chrome/119.0.0.0",
     "Accept-Language":"en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7"
@@ -21,11 +22,11 @@ if price <= 100:
     link = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
     with SMTP(host="smtp.gmail.com", port=587) as connection:
         connection.starttls()
-        connection.login(user="kobiljonovdavlatbek@gmail.com", password="qhnw grqg nmad ouas")
+        connection.login(user=YOUR_EMAIL, PASSWORD)
         connection.sendmail(
-            from_addr="kobiljonovdavlatbek@gmail.com",
+            from_addr=YOUR_EMAIL,
             msg=f"Subject:Amazon Price Alert!\n\n{product_name}\n{link}".encode('utf-8').strip(),
-            to_addrs="davlatkobiljonov@gmail.com",
+            to_addrs="some_email@gmail.com",
         )
     print("Message Sent!")
 
